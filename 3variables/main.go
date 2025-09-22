cffffd1c2bb6f68dc790815a984b676e120eb308
joel-2112 <eyueljoel21@gmail.com>
2025-09-22 19:20:33 +0300

    variables in go

---
+++ 3variables/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//declare and then assign in 2 steps
+	var mango string
+	mango = "mango"
+
+	//declare and assign in 1 step
+	var apple string = "apple"
+
+	//declare and assign with type inference
+	//declare and assign using :=
+	banana := "banana"
+	//output
+	fmt.Println("declare and assign in 2 line mango:", mango)
+	fmt.Println("declare and assign in 1 line apple:", apple)
+	fmt.Println("declare and assign using := banana:", banana)
+	//declare multiple variables in 1 line
+	var fruit1, fruit2, fruit3 string = "mango", "apple", "banana"
+	fmt.Println("declare multiple variables in 1 line",fruit1, fruit2, fruit3)
+	//declare multiple variables with type inference
+	var fruit4, fruit5, fruit6 = "mango", "apple", "banana"
+	fmt.Println("declare multiple variables with type inference",fruit4, fruit5, fruit6)
+	//declare multiple variables using :=
+	fruit7, fruit8, fruit9 := "mango", "apple", "banana"
+	fmt.Println("multiple variables using := ",fruit7, fruit8, fruit9)
+	//declare multiple variables in 1 line with different types
+	var fruit10, price, isGood = "mango", 20, true
+	fmt.Println("multiple variables in 1 line multiple type",fruit10, price, isGood)
+	//declare multiple variables using := with different types
+	fruit11, price1, isGood1 := "mango", 20, true
+	fmt.Println("multiple variables using := with different type",fruit11, price1, isGood1)
+	//swap values of 2 variables
+	mango, apple = apple, mango
+	fmt.Println("after swapping", mango, apple)
+
+}
