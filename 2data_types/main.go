d7f853e42a50fee962d2c74c42b31894a3efc6a4
joel-2112 <eyueljoel21@gmail.com>
2025-09-22 18:40:45 +0300

    basic data type and go mod

---
+++ 2data_types/main.go
@@ -0,0 +1,22 @@
+package main
+import "fmt"
+
+func main() {
+	var a int = 10
+	//used for less precise calculations
+	var ft32 float32 = 20.5
+	//used for more precise calculations
+	var ft64 float64 = 20.5
+	//default type for floating-point numbers is float64
+	var b = 20.5
+	var c string = "Hello, Go!"
+	var d bool = true
+	var e complex64 = 1 + 2i
+	fmt.Println("Integer:", a)
+	fmt.Println("Float32:", ft32)
+	fmt.Println("Float64:", ft64)
+	fmt.Println("Default Float:", b)
+	fmt.Println("String:", c)
+	fmt.Println("Boolean:", d)
+	fmt.Println("Complex Number:", e)
+}
