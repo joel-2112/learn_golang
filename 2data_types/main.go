addd2e2aa1a64015ea95023a61d5ece910fccf0e
joel-2112 <eyueljoel21@gmail.com>
2025-09-22 18:44:32 +0300

    demostrate float32 and float64 precision

--- 2data_types/main.go
+++ 2data_types/main.go
@@ -9,12 +9,18 @@ func main() {
 	var ft64 float64 = 20.5
 	//default type for floating-point numbers is float64
 	var b = 20.5
+	// demostrate the float32 and float64 types
+	var LP float32 = 34567839023444
+	var HP float64 = 34567839023444
+	fmt.Println("Low Precision:", LP)
+	fmt.Println("High Precision:", HP)
 	var c string = "Hello, Go!"
 	var d bool = true
 	var e complex64 = 1 + 2i
 	fmt.Println("Integer:", a)
 	fmt.Println("Float32:", ft32)
 	fmt.Println("Float64:", ft64)
+
 	fmt.Println("Default Float:", b)
 	fmt.Println("String:", c)
 	fmt.Println("Boolean:", d)
