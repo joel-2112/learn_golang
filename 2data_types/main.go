052b46bd2a3daa7804295e5b1a7cbdd08ceca08a
joel-2112 <eyueljoel21@gmail.com>
2025-09-22 19:24:41 +0300

    variables

--- 2data_types/main.go
+++ 2data_types/main.go
@@ -1,28 +1,32 @@
 package main
+
 import "fmt"
 
 func main() {
 	var a int = 10
-	//used for less precise calculations
-	var ft32 float32 = 20.5
-	//used for more precise calculations
-	var ft64 float64 = 20.5
 	//default type for floating-point numbers is float64
-	var b = 20.5
+	var b = 34567839023444
 	// demostrate the float32 and float64 types
 	var LP float32 = 34567839023444
 	var HP float64 = 34567839023444
 	fmt.Println("Low Precision:", LP)
 	fmt.Println("High Precision:", HP)
-	var c string = "Hello, Go!"
+	fmt.Println("Default Float:", b)
+	//default type for string is string
+	var name string = "Eyuel"
 	var d bool = true
 	var e complex64 = 1 + 2i
-	fmt.Println("Integer:", a)
-	fmt.Println("Float32:", ft32)
-	fmt.Println("Float64:", ft64)
-
-	fmt.Println("Default Float:", b)
-	fmt.Println("String:", c)
-	fmt.Println("Boolean:", d)
+	//demonstrater complex numbers
+	var cn1 complex128 = complex(5, 7)
+	var cn2 complex128 = 3 + 4i
+	var cnSum complex128 = cn1 + cn2
+	fmt.Println("Complex Number 1:", cn1)
+	fmt.Println("Complex Number 2:", cn2)
 	fmt.Println("Complex Number:", e)
+	fmt.Println("Sum of Complex Numbers:", cnSum)
+
+	fmt.Println("Integer:", a)
+
+	fmt.Println("My name is:", name)
+	fmt.Println("Boolean:", d)
 }
