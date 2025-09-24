a2506a2675d437489e6f608d1f0048678ef8dd14
joel-2112 <eyueljoel21@gmail.com>
2025-09-24 21:20:06 +0300

    global variables are not prioritise than local

--- 8scopes/main.go
+++ 8scopes/main.go
@@ -1,14 +1,20 @@
 package main
+
 import (
 	"fmt"
 )
-//global variable
+
+// global variable
 var z int
+var a = 100
+
 func main() {
 
 	//local variable
 	x := 30
 	y := 20
 	z = x + y
-	fmt.Printf( "x=%d, y=%d, global var z=%d", x, y, z)
+	var a = 200
+	fmt.Printf("local var a=%d\n", a)
+	fmt.Printf("x=%d, y=%d, global var z=%d\n", x, y, z)
 }
