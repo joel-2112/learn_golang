7470fd6413153c0138c0f196dfa614c4df10c3ea
joel-2112 <eyueljoel21@gmail.com>
2025-09-24 20:01:49 +0300

    formated return on functions

--- 7functions/main.go
+++ 7functions/main.go
@@ -1,23 +1,35 @@
 package main
-import ("fmt")
-func saycountry(s string ) {
-   fmt.Printf("%s, is a country\n",s)
+
+import (
+	"fmt"
+)
+
+func saycountry(s string) {
+	fmt.Printf("%s, is a country\n", s)
 }
-func saycity(s string){
-	fmt.Printf("%s, is a city\n",s)
+func saycity(s string) {
+	fmt.Printf("%s, is a city\n", s)
 }
-func maxoftwo(a, b int )int {
-	if a>b {
+func maxoftwo(a, b int) int {
+	if a > b {
 		return a
-	}else{
+	} else {
 		return b
 	}
 }
-
-func main(){
+func swap(firstName, lastName string) (string, string) {
+	return lastName, firstName
+}
+func main() {
 	fmt.Println("funtions")
 	saycountry("Ethiopia")
 	saycity("Addis Ababa")
-	var result = maxoftwo(3,5);
-	fmt.Println("max is ",result)
+	a, b := 300, 500
+	result := maxoftwo(a, b)
+	fmt.Println("max is ", result)
+	fmt.Printf("max of %d and %d is %d\n", a, b, result)
+	//when you declare multiple returns as string use long name not single letter
+	firstName, lastName  := swap("eyuel", "kassahun")
+	fmt.Printf("swapped : %s %s\n", firstName, lastName)
 }
+ 
