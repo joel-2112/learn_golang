92d3049abc5d3fa54d65903b3fc4710b60866be4
joel-2112 <eyueljoel21@gmail.com>
2025-09-24 20:21:13 +0300

    slices  as arguments

--- 7functions/main.go
+++ 7functions/main.go
@@ -20,6 +20,11 @@ func maxoftwo(a, b int) int {
 func swap(firstName, lastName string) (string, string) {
 	return lastName, firstName
 }
+//modify slices
+func modifyslice(s []int) {
+	s[0] = 100
+	s[1] = 200
+}
 func main() {
 	fmt.Println("funtions")
 	saycountry("Ethiopia")
@@ -31,5 +36,11 @@ func main() {
 	//when you declare multiple returns as string use long name not single letter
 	firstName, lastName  := swap("eyuel", "kassahun")
 	fmt.Printf("swapped : %s %s\n", firstName, lastName)
+
+	//slices
+	nums := []int{1, 2, 3, 4, 5}
+	fmt.Println("slice before modifying", nums)//1 2 3 4 5
+	modifyslice(nums)
+	fmt.Println("slice after modifying", nums)//100 200 3 4 5
 }
  
