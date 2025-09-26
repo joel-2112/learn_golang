c621742ee268a26062ae95dceea6ae654a025d71
joel-2112 <eyueljoel21@gmail.com>
2025-09-26 22:17:24 +0300

    error handling in go

---
+++ 19errorhandling/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+type error interface {
+	Error() string
+}
+//swuare root of the function
+
+func Sqrt(value float64) (float64, error) {
+	if value < 0 {
+		return 0, errors.New("negative number is passed to sqrt")
+	}
+	return math.Sqrt(value), nil
+}
+
+func main() {
+	fmt.Println("error handling in go")
+	result, err := Sqrt(16)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(result)
+	}
+
+}
